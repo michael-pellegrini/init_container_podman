16b7f0a90276f760ea3edf0433c5a2b14769c116
Michael Pellegrini <47049792+michael-pellegrini@users.noreply.github.com>
2022-03-20 20:52:01 -0400

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Check if postgres accepting connections"
+nc -vz localhost 5432
+status=$?
+
+while [ $status != 0 ]
+do
+  echo "sleeping for 5 seconds"
+  sleep 5
+  nc -vz localhost 5432
+  status=$?
+done
